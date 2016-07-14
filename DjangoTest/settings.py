f0f9480c375b0f4ae83bbfb2017b6d83a6a4b29a
Ali <ali.baig1996@gmail.com>
2016-07-14 09:57:50 +0500

    Removed dummy app from server

--- DjangoTest/settings.py
+++ DjangoTest/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Firstpp.apps.FirstppConfig',
     'TestApp.apps.TestappConfig', 
     'django.contrib.admin',
     'django.contrib.auth',
--- DjangoTest/urls.py
+++ DjangoTest/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
-	url(r'^first/', include('Firstpp.urls')),
 	url(r'^test/', include('TestApp.urls')),
 ]
